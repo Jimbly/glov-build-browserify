1d77965bc4b505bdb3ff5b6e20aeea31fdd3645d
Jimb Esser <wasteland@gmail.com>
2025-09-08 09:55:57 -0700

    Update test to deal with changes in sourcemap dependency; fix typo in test file

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ const { doTestList, multiTest } = require('./test_runner.js');
 
 const FILE_MAIN_JS = `
   const { test } = require('../common/foo.js');
-  const isInteger = require('is-finite');
+  const isFinite = require('is-finite');
   test(console.log.bind(console, isFinite(7.7), 'Done!'));
 `;
 const FILE_FOO_JS = `
@@ -24,7 +24,7 @@ module.exports = Number.isFinite || function (value) {
 var _require = require('../common/foo.js'),
     test = _require.test;
 
-var isInteger = require('is-finite');
+var isFinite = require('is-finite');
 
 test(console.log.bind(console, isFinite(7.7), 'Done!'));
 
@@ -44,7 +44,7 @@ function test(next) {
 
 //# sourceMappingURL=main.bundle.js.map
 `;
-const OUT_BUNDLE_MAP = `{"version":3,"sources":["node_modules/browser-pack/_prelude.js","node_modules/is-finite/index.js","main.js","../common/common/foo.js"],"names":[],"mappings":"AAAA;ACAA;AACA;AACA;AACA;AACA;AACA;;;;ACJE,eAAiB,OAAO,CAAC,kBAAD,CAAxB;AAAA,IAAQ,IAAR,YAAQ,IAAR;;AACA,IAAM,SAAS,GAAG,OAAO,CAAC,WAAD,CAAzB;;AACA,IAAI,CAAC,OAAO,CAAC,GAAR,CAAY,IAAZ,CAAiB,OAAjB,EAA0B,QAAQ,CAAC,GAAD,CAAlC,EAAyC,OAAzC,CAAD,CAAJ;;;;;;;;ACFO,SAAS,IAAT,CAAc,IAAd,EAAoB;AACzB,EAAA,OAAO,CAAC,GAAR,CAAY,OAAZ;AACA,EAAA,IAAI;AACL","file":"main.bundle.js","sourceRoot":"","sourcesContent":["(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c=\\"function\\"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error(\\"Cannot find module '\\"+i+\\"'\\");throw a.code=\\"MODULE_NOT_FOUND\\",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u=\\"function\\"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()","'use strict';\\n\\nmodule.exports = Number.isFinite || function (value) {\\n\\treturn !(typeof value !== 'number' || value !== value || value === Infinity || value === -Infinity);\\n};\\n","\\n  const { test } = require('../common/foo.js');\\n  const isInteger = require('is-finite');\\n  test(console.log.bind(console, isFinite(7.7), 'Done!'));\\n","\\n  export function test(next) {\\n    console.log('test!');\\n    next();\\n  }\\n"]}`;
+const OUT_BUNDLE_MAP = `{"version":3,"sources":["node_modules/browser-pack/_prelude.js","node_modules/is-finite/index.js","main.js","../common/foo.js"],"names":[],"mappings":"AAAA;ACAA;AACA;AACA;AACA;AACA;AACA;;;;ACJE,eAAiB,OAAO,CAAC,kBAAD,CAAxB;AAAA,IAAQ,IAAR,YAAQ,IAAR;;AACA,IAAM,QAAQ,GAAG,OAAO,CAAC,WAAD,CAAxB;;AACA,IAAI,CAAC,OAAO,CAAC,GAAR,CAAY,IAAZ,CAAiB,OAAjB,EAA0B,QAAQ,CAAC,GAAD,CAAlC,EAAyC,OAAzC,CAAD,CAAJ;;;;;;;;ACFO,SAAS,IAAT,CAAc,IAAd,EAAoB;AACzB,EAAA,OAAO,CAAC,GAAR,CAAY,OAAZ;AACA,EAAA,IAAI;AACL","file":"main.bundle.js","sourceRoot":"","sourcesContent":["(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c=\\"function\\"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error(\\"Cannot find module '\\"+i+\\"'\\");throw a.code=\\"MODULE_NOT_FOUND\\",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u=\\"function\\"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()","'use strict';\\n\\nmodule.exports = Number.isFinite || function (value) {\\n\\treturn !(typeof value !== 'number' || value !== value || value === Infinity || value === -Infinity);\\n};\\n","\\n  const { test } = require('../common/foo.js');\\n  const isFinite = require('is-finite');\\n  test(console.log.bind(console, isFinite(7.7), 'Done!'));\\n","\\n  export function test(next) {\\n    console.log('test!');\\n    next();\\n  }\\n"]}`;
 
 const FILE_DOUBLE_DEP = `
   const { test2 } = require('./delayed.js');
